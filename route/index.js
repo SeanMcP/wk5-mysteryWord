13275832c2893da36ba00fa07ebeff53f5bb24f1
Sean McPherson <seanmcp25@gmail.com>
2017-08-18 14:52:29 -0400

    Add play again button; begin style

--- route/index.js
+++ route/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 let words = ['alliance', 'barefoot', 'converse', 'dumfound', 'enormous'];
+let mdWords = ['ape', 'bat', 'can', 'dog']
+let smWords = ['z', 'x', 'a', 's'];
 
 function chooseWord(array){
   return array[Math.floor(Math.random() * array.length)];
@@ -10,6 +12,7 @@ function stringToObjArr (string){
   currentWordArr = [];
   for (var i = 0; i < string.length; i++) {
     let obj = {
+      actual: string[i],
       value: string[i],
       placeholder: "_",
       guessed: false
@@ -51,22 +54,6 @@ router.post('/guess', function(req, res){
       }
     })
 
-    // function isCorrect(obj){
-    //   return obj.guessed = true;
-    // };
-    // if(currentWordArr.every(isCorrect)){
-    //   console.log('True');
-    // } else {
-    //   console.log('False');
-    // }
-
-    // let winCheck = false;
-    // currentWordArr.forEach(function(letter){
-    //   console.log(winCheck);
-    //   if (!letter.guessed) break;
-    //   winCheck = true;
-    //   console.log(winCheck);
-    // })
     let winCheck = false;
     for(let letter of currentWordArr){
       if(letter.guessed == false){
@@ -75,16 +62,66 @@ router.post('/guess', function(req, res){
       }
       winCheck = true;
     }
+
+    // **********************************
+    //             YOU WIN!
+    // **********************************
     if(winCheck){
       console.log('You win!');
+      dataObj.outcome = 'You win!';
+      if(dataObj.remaining !== 1){
+        dataObj.message = 'With ' +  dataObj.remaining + ' moves to spare';
+      } else {
+        dataObj.message = 'With ' +  dataObj.remaining + ' move to spare';
+      }
+      req.session.answer = dataObj;
+      console.log('dataObj:\n', dataObj);
+      res.redirect('/over');
     }
 
     if(!guessFlag){
       dataObj.remaining--;
     }
+
+    // **********************************
+    //             YOU LOSE!
+    // **********************************
     guesses.push(req.body.guess);
+    if(dataObj.remaining < 0){
+      console.log('You lose!');
+      dataObj.outcome = 'You lose!';
+      dataObj.message = 'Better luck next time';
+      dataObj.remaining = 0;
+      req.session.answer = dataObj;
+      res.redirect('/over');
+    }
     res.redirect('/');
   }
+});
+
+router.get('/over', function(req, res){
+  res.render('over', {session: req.session.answer})
+  console.log(req.session.answer);
+});
+
+router.get('/again', function(req, res){
+
+  req.session.destroy(function(err){
+    console.log('Plag again error:', err);
+  })
+
+  currentWordArr = [];
+  stringToObjArr(chooseWord(words));
+
+  guesses = [];
+  remaining = currentWordArr.length;
+
+  dataObj = {
+    word: currentWordArr,
+    guesses: guesses,
+    remaining: currentWordArr.length
+  }
+  res.redirect('/');
 });
 
 module.exports = router;
