481520597d0a40f3c7e0da7e25c829828e850119
Sean McPherson <seanmcp25@gmail.com>
2017-08-17 23:13:11 -0400

    Create function to turn string into object-array with value and placeholder; logic to swap placeholder and value when match occurs; Degrade number of guesses remaining only for wrong guess

--- route/index.js
+++ route/index.js
@@ -1,8 +1,54 @@
 const express = require('express');
 const router = express.Router();
 
+let words = ['redbeard'];
+let formattedWord = ['r', 'e', 'd', 'b', 'e', 'a', 'r', 'd'];
+let testString = 'redbeard';
+let testArr = [];
+
+for (var i = 0; i < testString.length; i++) {
+  let obj = {
+    value: "_",
+    placeholder: testString[i]
+  }
+  testArr.push(obj);
+}
+
+console.log('testArr after:', testArr);
+
+let guesses = [];
+let remaining = 8;
+
+let dataObj = {
+  word: testArr,
+  guesses: guesses,
+  remaining: remaining
+}
+
 router.get('/', function(req, res){
-  res.render('layout')
+  res.render('layout', dataObj);
+});
+
+router.post('/guess', function(req, res){
+  let guessFlag = false;
+  let newGuess = req.body.guess;
+  if(guesses.includes(newGuess)){
+    guessFlag = true;
+    return;
+  } else {
+    testArr.forEach(function(obj){
+      if(obj.placeholder == newGuess){
+        guessFlag = true;
+        obj.placeholder = obj.value;
+        obj.value = newGuess;
+      }
+    })
+    if(!guessFlag){
+      dataObj.remaining--;
+    }
+    guesses.push(req.body.guess);
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
