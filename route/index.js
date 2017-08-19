be0c196ec44b459b7148671c359b5c986355885d
Sean McPherson <seanmcp25@gmail.com>
2017-08-19 16:39:56 -0400

    Fix get request error on /over; add negative lateral margins on #wordField

--- route/index.js
+++ route/index.js
@@ -4,9 +4,9 @@ const router = express.Router();
 
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
 
-let realWords = ['alliance', 'barefoot', 'converse', 'dumfound', 'enormous'];
+let easyWords = ['alliance', 'barefoot', 'converse', 'dumfound', 'enormous'];
 
-function chooseWord(array){
+function chooseWord (array) {
   return array[Math.floor(Math.random() * array.length)];
 };
 function stringToObjArr (string){
@@ -30,7 +30,7 @@ function createGameState(){
   }
 }
 
-router.get('/', function(req, res){
+router.get('/', function (req, res) {
   if(!req.session.state){
     req.session.state = createGameState();
     res.render('game', req.session.state);
@@ -39,12 +39,12 @@ router.get('/', function(req, res){
   }
 });
 
-router.post('/guess', async function(req, res){
+router.post('/guess', async function (req, res) {
   let currentState = req.session.state;
 
   req.checkBody('guess', 'Guess a letter').notEmpty();
   req.checkBody('guess', 'Only guess one letter at a time').len(1, 1);
-  req.checkBody('guess', 'Your guesses can only be letters').isAlpha();
+  req.checkBody('guess', 'You can only guess English letters').isAlpha();
 
   let errors = await req.getValidationResult();
   let messages = errors.array().map(function(error){
@@ -53,15 +53,15 @@ router.post('/guess', async function(req, res){
 
   currentState.error = messages;
 
-  if(currentState.error.length === 0){
+  if (currentState.error.length === 0) {
     let guessFlag = false;
     let newGuess = req.body.guess.toLowerCase();
-    if(currentState.guesses.includes(newGuess)){
+    if (currentState.guesses.includes(newGuess)) {
       guessFlag = true;
       return;
     } else {
-      currentState.word.forEach(function(obj){
-        if(obj.value == newGuess){
+      currentState.word.forEach(function (obj) {
+        if (obj.value == newGuess) {
           guessFlag = true;
           obj.placeholder = obj.value;
           obj.value = "_";
@@ -70,8 +70,8 @@ router.post('/guess', async function(req, res){
       })
 
       let winCheck = false;
-      for(let letter of currentState.word){
-        if(letter.guessed == false){
+      for (let letter of currentState.word) {
+        if (letter.guessed == false) {
           winCheck = false;
           break;
         }
@@ -81,9 +81,9 @@ router.post('/guess', async function(req, res){
       // **********************************
       //             YOU WIN!
       // **********************************
-      if(winCheck){
+      if (winCheck) {
         currentState.outcome = 'You win,';
-        if(currentState.remaining !== 1){
+        if (currentState.remaining !== 1) {
           currentState.message = 'with ' +  currentState.remaining + ' guesses to spare!';
         } else {
           currentState.message = 'with ' +  currentState.remaining + ' guesses to spare!';
@@ -91,7 +91,7 @@ router.post('/guess', async function(req, res){
         res.redirect('/over');
       }
 
-      if(!guessFlag){
+      if (!guessFlag) {
         currentState.remaining--;
       }
 
@@ -99,7 +99,7 @@ router.post('/guess', async function(req, res){
       //             YOU LOSE!
       // **********************************
       currentState.guesses.push(req.body.guess.toLowerCase());
-      if(currentState.remaining < 1){
+      if (currentState.remaining < 1) {
         currentState.outcome = 'You lose.';
         currentState.message = 'Better luck next time.';
         currentState.remaining = 0;
@@ -115,7 +115,7 @@ router.post('/guess', async function(req, res){
 router.get('/over', function(req, res){
   if (!req.session.state) {
     res.redirect('/')
-  } else if (req.session.state.remaining > 0) {
+  } else if (!req.session.state.outcome) {
     res.redirect('/')
   } else {
     res.render('over', {session: req.session.state})
@@ -125,7 +125,9 @@ router.get('/over', function(req, res){
 router.get('/again', function(req, res){
 
   req.session.destroy(function(err){
-    console.log('Play again error:', err);
+    if (err) {
+      console.log('Play again error:', err);
+    }
   })
   res.redirect('/');
 });
